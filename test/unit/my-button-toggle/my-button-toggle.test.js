860aabd672539dac5b87ddfff1fc000e2605b048
Peter Culak <peter.culak@worldfirst.com>
2018-10-22 18:43:08 +0100

    Button toggle tests

--- test/unit/my-button-toggle/my-button-toggle.test.js
+++ test/unit/my-button-toggle/my-button-toggle.test.js
@@ -1,29 +1,4 @@
-/**
- * @license
- * Copyright 2018 Google Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-
 import {assert} from 'chai';
-import td from 'testdouble';
 import bel from 'bel';
 import {MyButtonToggle, MyRedblueToggleFoundation} from '../../../packages/my-button-toggle/index';
 
@@ -35,15 +10,38 @@ function getFixture() {
   `;
 }
 
-function setupTest(root = getFixture()) {
-  const MockFoundationCtor = td.constructor(MyRedblueToggleFoundation);
-  const mockFoundation = new MockFoundationCtor();
-  const component = new MyButtonToggle(root, mockFoundation);
-  return {root, component, mockFoundation};
+function getFixtureAriaPressed() {
+  return bel`
+  <div class="redblue-toggle" role="button" aria-pressed="true">
+  Toggle <span class="redblue-toggle__color">Red</span>
+  </div>
+  `;
 }
 
 suite('MYButtonToggle');
 
 test('attachTo initializes and returns a MyButtonToggle instance', () => {
   assert.isTrue(MyButtonToggle.attachTo(getFixture()) instanceof MyButtonToggle);
+});
+
+test('getToggled returns false when aria-pressed is not set', () => {
+  const component = MyButtonToggle.attachTo(getFixture());
+  assert.isFalse(component.toggled);
+});
+
+test('getToggled returns true when aria-pressed is set', () => {
+  const component = MyButtonToggle.attachTo(getFixtureAriaPressed());
+  assert.isTrue(component.toggled);
+});
+
+test('setToggled sets toggled state from false to true', () => {
+  const component = MyButtonToggle.attachTo(getFixture());
+  assert.isFalse(component.toggled);
+  component.toggled = true;
+  assert.isTrue(component.toggled);
+});
+
+test('getDefaultFoundation returns instance of MyRedblueToggleFoundation', () => {
+  const component = MyButtonToggle.attachTo(getFixture());
+  assert.isTrue(component.getDefaultFoundation() instanceof MyRedblueToggleFoundation);
 });
