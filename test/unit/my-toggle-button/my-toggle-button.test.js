43ffb7558dfe16f5adc9fa8d3fc7207626a9e879
Peter Culak <peter.culak@worldfirst.com>
2018-10-24 20:01:16 +0100

    Toggle button test

--- test/unit/my-toggle-button/my-toggle-button.test.js
+++ test/unit/my-toggle-button/my-toggle-button.test.js
@@ -1,17 +1,47 @@
 import {assert} from 'chai';
 import bel from 'bel';
-import {MYToggleButton} from '../../../packages/my-toggle-button/index';
+import {MYToggleButton, MYToggleButtonFoundation} from '../../../packages/my-toggle-button/index';
 
 function getFixture() {
   return bel`
   <div class="redblue-toggle" role="button" aria-pressed="false">
-    Toggle <span class="redblue-toggle__color">Blue</span>
+  Toggle <span class="redblue-toggle__color">Blue</span>
   </div>
   `;
 }
 
-// suite('MDCToggleButton');
+function getFixtureAriaPressed() {
+  return bel`
+  <div class="redblue-toggle" role="button" aria-pressed="true">
+  Toggle <span class="redblue-toggle__color">Red</span>
+  </div>
+  `;
+}
 
-test('attachTo initializes and returns a MYToggleButton instance', () => {
+// suite('MYToggleButton');
+
+test('attachTo initializes and returns a MyButtonToggle instance', () => {
   assert.isTrue(MYToggleButton.attachTo(getFixture()) instanceof MYToggleButton);
+});
+
+test('getToggled returns false when aria-pressed is not set', () => {
+  const component = MYToggleButton.attachTo(getFixture());
+  assert.isFalse(component.toggled);
+});
+
+test('getToggled returns true when aria-pressed is set', () => {
+  const component = MYToggleButton.attachTo(getFixtureAriaPressed());
+  assert.isTrue(component.toggled);
+});
+
+test('setToggled sets toggled state from false to true', () => {
+  const component = MYToggleButton.attachTo(getFixture());
+  assert.isFalse(component.toggled);
+  component.toggled = true;
+  assert.isTrue(component.toggled);
+});
+
+test('getDefaultFoundation returns instance of MyRedblueToggleFoundation', () => {
+  const component = MYToggleButton.attachTo(getFixture());
+  assert.isTrue(component.getDefaultFoundation() instanceof MYToggleButtonFoundation);
 });
